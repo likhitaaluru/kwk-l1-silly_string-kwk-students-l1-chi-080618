8e8589378c06077ec86e6736871c8c61e6a37a02
likhitaaluru <likhitaaluru@gmail.com>
2018-08-07 14:40:05 +0000

    automatically backed up by learn

--- README.md
+++ README.md
@@ -11,15 +11,15 @@ You can run `learn` to see if the tests are passing.
 ## Challenge:
 
 1. Create a string that contains your favorite word. It can be in any language!
-
+puts "chocolate"
 2. Using a method built into ruby, capitalize every letter in the string you just created
-
+puts "chocolate".upcase
 3. Now let's make all the characters lowercase in the string `"IM NOT SHOUTING"`
-
+puts "IM NOT SHOUTING".downcase
 4. Using a method built into ruby, count how many letters are in the string `"supercalifragilisticexpialidocious"`
-
+puts "supercalifragilisticexpialidocious".length
 5. Now let's add more words to that string, using `+` just like in math. That's called concatenation. Let's add together to the two strings `"wow"` and `"mom"`.
-
+puts "supercalifragilisticexpialidocious+wow+mom"
 6. Now let's capitalize just the first letter of the sentence `"i really like programming"`
 
 7. Let's take the string `"Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"`. We want to use the `gsub` method to switch all `A` for `O`, as well as all `a` for `o`. We'll need to chain `gsub`s.
--- silly_string.rb
+++ silly_string.rb
@@ -1,13 +1,13 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = ""
+your_favorite_word = "onomatopoeia"
 
 puts "My favorite word is #{your_favorite_word}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
-favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
+favorite_word_in_all_caps = onomatopoeia.upcase # Put a . after your_favorite word and use a String method.
 
 puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
 
@@ -15,13 +15,16 @@ puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
 
 phrase = "IM NOT SHOUTING"
 
+
 lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
 puts lowercased_phrase
+puts "IM NOT SHOUTING".downcase
 
 # Challenge 4: Count how many letters are in that big_word
 
 big_word = "supercalifragilisticexpialidocious"
+puts "supercalifragilisticexpialidocious".length
 
 letter_count = big_word # Put a . after big_word and use a String method.
 
@@ -32,9 +35,10 @@ puts "There are #{letter_count} in #{big_word}"
 bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
 
 new_letter_count = bigger_word.size
+new_letter_count = 
 
 puts "There are now #{new_letter_count} in #{bigger_word}"
-
+puts ""
 # Challenge 6: Capitalize this sentence properly
 
 proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
